8c97e8fe5c9756fc1bf699fd1316875b84bee88e
Alejandro Velardo <a.velardo.lucero@gmail.com>
2023-05-15 09:44:28 +0200

    LOGIN configurado y middleware de rutas protegidas creado

---
+++ controllers/middleware.js
@@ -0,0 +1,30 @@
+import jsonwebtoken from "jsonwebtoken"
+
+
+export const authenticate = (req, res, next) => {
+	let token = req.headers.authorization || '';
+	if (!token) {
+		next({ error: 'No token in request' });
+	} 
+
+	jsonwebtoken.verify(token, process.env.SECRET_KEY, (error, decoded) => {
+		if (error) {
+			next({ error: 'Invalid token' });
+		} else {
+			let { expiredAt } = decoded;
+			if (Number(expiredAt) > new Date().getTime()) {
+				next();
+			} else {
+				next({ error: 'Session expired'});
+			}
+		}
+	});
+
+};
+
+export const authError = (err, req, res, next) => {
+	console.log("Authentication error")
+	console.log(err)
+	res.status(400).json(err.error);
+};
+
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,15 +1,18 @@
 import express from 'express'
 import { sequelize } from "../config/loadSequelize.js"
 import { User } from '../models/User.js'
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt'
+import jsonwebtoken from 'jsonwebtoken'
 
 const router = express.Router()
 
 // POST user
-// @desc register user in DB with encripted password 
+// @desc REGISTER user in DB with encripted password 
 router.post("/register", (req, res) => {
+    const { password, repeatpassword } = req.body;
+
     sequelize.sync().then(() => {
-        if (req.body.password == req.body.repeatpassword) {
+        if (password == repeatpassword) {
             
             const hash = bcrypt.hashSync(req.body.password, 10)
 
@@ -39,6 +42,50 @@ router.post("/register", (req, res) => {
 
     })
 })
+
+// POST user
+// @desc LOG IN user and return a token 
+router.post("/login", (req, res) => {
+    const response = {}
+    const { email, password } = req.body
+    if (!email || !password) {
+        return res.status(400).json({
+            ok: false,
+            error: "Email or password not received."
+        });
+    }
+
+    sequelize.sync().then(() => {
+       
+        User.findOne({ where: { email } })
+        .then((user) => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                return user;
+            } else {
+                throw "Incorrect email or password.";
+            }
+        })
+        .then(user => {
+            response.token = jsonwebtoken.sign(
+                {
+                    expiredAt: new Date().getTime() + Number(process.env.EXPIRED_AFTER),
+                    email: user.email,
+                },
+                process.env.SECRET_KEY
+            );
+            response.ok = true;
+            console.log(response)
+            res.json(response);
+        })
+        .catch(err => res.status(400).json({
+            ok: false,
+            error: err
+        }))
+
+
+    })
+})
+
 
 
 export default router
